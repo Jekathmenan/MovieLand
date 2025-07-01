7849dd40f71021eecd893c80af0431b48b048d82
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-07-01 17:55:24 +0200

    App.js with main logic
    API Request and Movie Cards display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Importing required components
+import { Movie, Search } from './components/';
+
+// Declaring required API Constants
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY; //'642180a4';
+const API_URL = process.env.REACT_APP_OMDB_API_URL + API_KEY
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        setMovies(data.Search)
+    }
+
+    useEffect(() => {
+
+    }, []);
+    return (
+
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <Search searchMovies={searchMovies} />
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="movies">
+                            {
+                                movies.map(
+                                    (movie) => <Movie key={movie.imdbID} movie={movie} />
+                                )
+                            }
+
+                        </div>
+                    )
+                    : (
+                        <div><h2>No movies found!</h2></div>
+                    )
+            }
+
+        </div >
+    );
 }
 
 export default App;
